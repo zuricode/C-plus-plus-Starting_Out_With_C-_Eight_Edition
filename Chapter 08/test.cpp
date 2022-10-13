06568ac170ea64c5a4425622ae5c27a66f90ea86
alphaman9 <zurijb@gmail.com>
2022-10-13 17:20:54 +0100

    Update test.cpp

--- Chapter 08/test.cpp
+++ Chapter 08/test.cpp
@@ -3,34 +3,29 @@
 using namespace std;
 
 void displayArray(int[], int);
-void swap(int&, int&);
 void sortArray(int[], int);
+void swap(int&, int&);
 
 int main() {
 
-	const int			SIZE{ 15};
-	int					list[SIZE]{ 34,67,21,45,63,4564,2,45,12,6594, -56675, -567, 5675636, 10, 999 };
+	const int			SIZE{ 15 };
+	int					list[SIZE]{ 56,4,8,32,345,2, -657, 5642, 1, 45, 6874, 34534734, -569874, -4563487, 564 };
 
 	displayArray(list, SIZE);
 	sortArray(list, SIZE);
 	displayArray(list, SIZE);
 
-
 }
 
 void displayArray(int array[], int size) {
 
-	cout << "List of numbers: ";
-
 	for (int i = 0; i < size; i++) {
-
 		if (i != size - 1) {
 			cout << array[i] << " ";
 		}
 		else {
 			cout << array[i] << endl;
 		}
-
 	}
 
 	cout << endl;
@@ -43,8 +38,8 @@ void sortArray(int array[], int size) {
 
 	for (int start = 0; start < size - 1; start++) {
 
-		min_value = array[start];
 		min_index = start;
+		min_value = array[start];		
 
 		for (int index = start + 1; index < size; index++) {
 
@@ -55,9 +50,8 @@ void sortArray(int array[], int size) {
 
 		}
 
-		swap(array[min_index], array[start]);
-
-
+	swap(array[start], array[min_index]);
+	
 	}
 
 }
