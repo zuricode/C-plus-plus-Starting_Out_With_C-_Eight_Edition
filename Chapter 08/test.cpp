cc35243e5df29dcfc657a01269ff8ee12c2a6a89
alphaman9 <60569903+alphaman9@users.noreply.github.com>
2022-10-18 17:31:17 +0100

    Update test.cpp

--- Chapter 08/test.cpp
+++ Chapter 08/test.cpp
@@ -1,66 +1,13 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-void displayArray(int[], int);
-void sortArray(int[], int);
-void swap(int&, int&);
-
 int main() {
 
-	const int			SIZE{ 15 };
-	int					list[SIZE]{ 56,4,8,32,345,2, -657, 5642, 1, 45, 6874, 34534734, -569874, -4563487, 564 };
-
-	displayArray(list, SIZE);
-	sortArray(list, SIZE);
-	displayArray(list, SIZE);
-
-}
-
-void displayArray(int array[], int size) {
-
-	for (int i = 0; i < size; i++) {
-		if (i != size - 1) {
-			cout << array[i] << " ";
-		}
-		else {
-			cout << array[i] << endl;
-		}
-	}
-
-	cout << endl;
-
-}
-
-void sortArray(int array[], int size) {
-
-	int min_index, min_value;
-
-	for (int start = 0; start < size - 1; start++) {
-
-		min_index = start;
-		min_value = array[start];		
-
-		for (int index = start + 1; index < size; index++) {
-
-			if (array[index] < min_value) {
-				min_index = index;
-				min_value = array[index];
-			}
-
-		}
-
-	swap(array[start], array[min_index]);
+	string file_name {"C:\Users\test\Documents\GitHub\C - plus - plus - Starting_Out_With_C - _Eight_Edition\Chapter 08"};
 	
-	}
-
-}
-
-void swap(int& a, int& b) {
-
-	int temp;
-	temp = a;
-	a = b;
-	b = temp;
+	cout << file_name << endl << endl;
+	
 
 }
