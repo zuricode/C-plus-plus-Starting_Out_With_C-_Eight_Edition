8fc42a3dd6b870dc63b9e4ee3c88600a591eb7e6
alphaman9 <zurijb@gmail.com>
2022-10-25 19:55:30 +0100

    Update 02 - Test Scores #1.cpp

--- Chapter 09 - Pointers/02 - Test Scores #1.cpp
+++ Chapter 09 - Pointers/02 - Test Scores #1.cpp
@@ -2,21 +2,34 @@
 
 using namespace std;
 
-double inputValidation(double);
-double* allocateMemory();
+int inputValidation(int);
+double* allocateMemory(int&);
+void enterTestScores(double *, int);
+void sortTestScores(double*, int);
+void swap_double(double&, double&);
+void showTestScores(double*, int, double);
+double getAverage(double*, int);
 
 int main() {
 
-	double* ptr = allocateMemory();
+	int test_scores;
+	double average;
+	double* ptr = allocateMemory(test_scores);
 
+	enterTestScores(ptr, test_scores);
+	sortTestScores(ptr, test_scores);
+	average = getAverage(ptr, test_scores);
+
+	showTestScores(ptr, test_scores, average);
 
 	delete[] ptr;
 	ptr = nullptr;
+
 }
 
-double inputValidation(double limit) {
+int inputValidation(int limit) {
 
-	int num;
+	double num;
 
 	while (!(cin >> num) || num < limit) {
 		cout << "ERROR: Value must be an integer greater than" << limit << "Re-enter your value: ";
@@ -28,16 +41,92 @@ double inputValidation(double limit) {
 
 }
 
-double* allocateMemory() {
+double* allocateMemory(int& test_scores) {
 
 	cout << "How many test scores would you like to collect? ";
 
-	double num = inputValidation(0);
+	test_scores = inputValidation(0);
 
-	cout << "Memory has been allocated for " << num << " test scores." << endl << endl;
+	cout << "Memory has been allocated for " << test_scores << " test scores." << endl << endl;
 
-	return new double[num];	
+	return new double[test_scores];	
 
+}
+
+void enterTestScores(double* ptr, int size) {
+
+	double num;
+
+	for (int i = 0; i < size; i++) {
+
+		cout << "Enter test score #" << i + 1 << ": ";
+		num = inputValidation(0);
+		*(ptr + i) = num;
+
+	}
+
+	cout << endl;
+
+}
+
+void sortTestScores(double* ptr, int size) {
+
+	double max_value;
+	int max_index;
+
+	for (int start = 0; start < size - 1; start++) {
+		max_index = start;
+		max_value = *(ptr + start);
+		for (int index = start + 1; index < size; index++) {
+			if (*(ptr + index) > max_value) {
+				max_value = *(ptr + index);
+				max_index = index;
+			}
+		}
+
+		swap_double(*(ptr + start), *(ptr + max_index));
+
+	}
+
+
+}
+
+void swap_double(double& a, double& b) {
+
+	double temp;
+	temp = a;
+	a = b;
+	b = temp;
+
+}
+
+void showTestScores(double* ptr, int size, double average) {
+
+	cout << "TEST SCORES: " << endl;
+	cout << "------------------" << endl << endl;
+
+	for (int i = 0; i < size; i++) {
+
+		cout << "Test score #" << i + 1 << ": " << *(ptr + i) << endl;
+
+	}
+
+	cout << endl;
+
+	cout << "The average score from " << size << " tests is " << average << endl;
+
+}
+
+double getAverage(double* ptr, int size) {
+
+	double total = 0;
+
+	for (int i = 0; i < size; i++) {
+		total += *(ptr + i);
+	}
+
+	return total / size;
+		 
 }
 
 
