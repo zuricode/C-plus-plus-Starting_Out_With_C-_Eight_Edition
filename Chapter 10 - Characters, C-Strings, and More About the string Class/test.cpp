c86e62b61803cf7a8f4ff415c208e8b1a27444b2
alphaman9 <zurijb@gmail.com>
2022-11-10 22:42:27 +0000

    Create test.cpp

---
+++ Chapter 10 - Characters, C-Strings, and More About the string Class/test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+	string str1 = "Actee";
+	string str2 = "Ackee";
+
+	if (str1 == str2) {
+
+		cout << "They are the same!" << endl << endl;
+
+	}
+
+}
