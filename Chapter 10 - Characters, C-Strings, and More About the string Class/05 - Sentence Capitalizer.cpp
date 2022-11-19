640604957207a9573f01e7d799ad716d8bef8129
alphaman9 <60569903+alphaman9@users.noreply.github.com>
2022-11-19 23:25:58 +0000

    Update 05 - Sentence Capitalizer.cpp

--- Chapter 10 - Characters, C-Strings, and More About the string Class/05 - Sentence Capitalizer.cpp
+++ Chapter 10 - Characters, C-Strings, and More About the string Class/05 - Sentence Capitalizer.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 void capitalizeText(char*);
+void capitalizeText(&string);
+
 
 int main() {
 
 	const int		SIZE = 101;
+	string			text_string;
 	char			text[SIZE];
 
 
@@ -18,6 +22,17 @@ int main() {
 	capitalizeText(text);
 
 	cout << "Capitalized: " << text << endl;
+
+	cout << endl << endl;
+
+	cout << "Enter your string (no more than " << SIZE << " characters): ";
+	getline(cin, text_string);
+
+	cout << "Original: " << text_string << endl;
+
+	capitalizeText(text_string);
+
+	cout << "Capitalized: " << text_string << endl;
 
 	cout << endl;
 
@@ -34,12 +49,28 @@ void capitalizeText(char* ptr) {
 		if (i == 0) {
 			*(ptr + i) = toupper(*(ptr + i));
 		}
-
-		if ((*(ptr + i) == '.' || *(ptr + i) == '!') && *(ptr + (i + 1)) != '\0') {
+		
+		if ((*(ptr + i) == '.' || *(ptr + i) == '!' || *(ptr + i) == '?') && *(ptr + (i + 1)) != '\0') {
 			*(ptr + (i + 2)) = toupper(*(ptr + (i + 2)));
 		}
 
 		i++;
+
+	}
+
+}
+
+void capitalizeText(&string text) {
+
+	for (int i = 0; i < text.size(); i++) {
+
+				if (i == 0) {
+					text[i] = toupper(text[i]);
+				}
+
+				if ((text[i] == '.' || text[i] == '!' || text[i] == '?') && text[i + 1] != '\0') {
+					text[i + 2] = toupper(text[i + 2]);
+				}
 
 	}
 
