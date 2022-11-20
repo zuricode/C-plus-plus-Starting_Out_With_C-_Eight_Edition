3454bcb96c8b1167c32943ff06bcbc36ea5359e7
alphaman9 <60569903+alphaman9@users.noreply.github.com>
2022-11-20 11:34:36 +0000

    Update 08 - Sum of Digits in a String.cpp

--- Chapter 10 - Characters, C-Strings, and More About the string Class/08 - Sum of Digits in a String.cpp
+++ Chapter 10 - Characters, C-Strings, and More About the string Class/08 - Sum of Digits in a String.cpp
@@ -1,3 +1,10 @@
+//8. Sum of Digits in a String
+//Write a program that asks the user to enter a series of single digit numbers with nothing
+//separating them.Read the input as a C - string or a string object.The program should
+//display the sum of all the single - digit numbers in the string.For example, if the user
+//enters 2514, the program should display 12, which is the sum of 2, 5, 1, and 4. The
+//program should also display the highest and lowest digits in the string.
+
 #include <iostream>
 #include <string>
 
