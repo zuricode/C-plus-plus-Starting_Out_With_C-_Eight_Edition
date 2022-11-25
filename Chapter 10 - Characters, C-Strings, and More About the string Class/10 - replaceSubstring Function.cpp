90c7f4ed98b3e6509913ba24458b10b574063f6d
alphaman9 <zurijb@gmail.com>
2022-11-25 14:13:09 +0000

    Update 10 - replaceSubstring Function.cpp

--- Chapter 10 - Characters, C-Strings, and More About the string Class/10 - replaceSubstring Function.cpp
+++ Chapter 10 - Characters, C-Strings, and More About the string Class/10 - replaceSubstring Function.cpp
@@ -1,3 +1,13 @@
+//5. Sentence Capitalizer
+//Write a function that accepts a pointer to a C - string as an argument and capitalizes
+//the first character of each sentence in the string.For instance, if the string argument
+//is “ hello.my name is Joe.what is your name ? ” the function should manipulate
+//the string so it contains “ Hello.My name is Joe.What is your name ? ”
+//Demonstrate the function in a program that asks the user to input a string and then
+//passes it to the function.The modified string should be displayed on the screen.
+//Optional Exercise : Write an overloaded version of this function that accepts a string
+//class object as its argument.
+
 #include <iostream>
 
 using namespace std;
