577c3bd87223cae6a7a0e9322ddf824f84ce253f
alphaman9 <zurijb@gmail.com>
2022-11-25 16:42:47 +0000

    Update test.cpp

--- Chapter 10 - Characters, C-Strings, and More About the string Class/test.cpp
+++ Chapter 10 - Characters, C-Strings, and More About the string Class/test.cpp
@@ -5,13 +5,18 @@ using namespace std;
 
 int main() {
 
-	string str1 = "Actee";
-	string str2 = "Ackee";
+	int position;
 
-	if (str1 == str2) {
+	string string1 = "the cat sat on the cat mat";
+	string string2 = "cat";
+	string string3 = "dobvnjgfhfdhgfg";
 
-		cout << "They are the same!" << endl << endl;
+	cout << "Original phrase: " << string1 << endl;
 
-	}
+	position = string1.find(string2);
+
+	string1.replace(position, string2.size(), string3);
+
+	cout << "Replacement phrase: " << string1 << endl;
 
 }
