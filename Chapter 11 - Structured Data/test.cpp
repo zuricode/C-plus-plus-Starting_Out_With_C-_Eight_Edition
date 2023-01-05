39612ac107236db8d3da1034f73af8ff9a720db3
alphaman9 <zurijb@gmail.com>
2023-01-05 17:42:06 +0000

    Create test.cpp

---
+++ Chapter 11 - Structured Data/test.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+#include<iomanip>
+
+struct TempScale {
+
+	double fahrenheit;
+	double centigrade;
+
+};
+
+struct Reading {
+
+	int windSpeed;
+	double humidity;
+	TempScale temperature;
+
+};
+
+using namespace std;
+
+int main() {
+
+	enum { DOG, CAT, BIRD, HAMSTER };
+
+}
