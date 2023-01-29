1d650cb2b537b835f3b8562fd4f5cf173fa362a2
alphaman9 <zurijb@gmail.com>
2023-01-29 00:58:28 +0000

    Create test.cpp

---
+++ Chapter 12 - Advanced File Operations/test.cpp
@@ -0,0 +1,21 @@
+﻿#include<fstream>
+#include<iostream>
+
+
+using namespace std;
+
+int main() {
+
+	fstream dataFile("info.dat", ios::out | ios::binary);
+
+	struct Date
+	{
+		int month;
+		int day;
+		int year;
+	};
+
+	Date dt = { 4, 2, 98 };
+	dataFile.write(reinterpret_cast<char *>(&dt), sizeof(dt));
+
+}
