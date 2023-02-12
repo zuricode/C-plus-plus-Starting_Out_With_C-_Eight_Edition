a60f1d9299a7cd1d728cfe6ba106647ec3afa3cb
alphaman9 <zurijb@gmail.com>
2023-02-12 17:01:17 +0000

    Update test.cpp

--- Chapter 12 - Advanced File Operations/test.cpp
+++ Chapter 12 - Advanced File Operations/test.cpp
@@ -1,21 +1,47 @@
-﻿#include<fstream>
-#include<iostream>
-
+﻿// This program uses the write and read functions.
+#include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main() {
 
-	fstream dataFile("info.dat", ios::out | ios::binary);
+	const int SIZE = 10;
+	fstream file;
+	int numbers[SIZE] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	int new_numbers[SIZE];
 
-	struct Date
-	{
-		int month;
-		int day;
-		int year;
-	};
+	// Display the contents of the array.
+	for (int count = 0; count < SIZE; count++)
+		cout << numbers[count] << " ";
+	cout << endl;
 
-	Date dt = { 4, 2, 98 };
-	dataFile.write(reinterpret_cast<char *>(&dt), sizeof(dt));
+	cout << endl;
+	
+	// Open the file for output in binary mode.
+	file.open("numbers.dat", ios::out | ios::binary);
+
+	// Write the contents of the array to the file.
+	cout << "Writing the data to the file.\n";
+	file.write(reinterpret_cast<char*>(numbers), sizeof(numbers));
+
+	// Close the file.
+	file.close();
+
+	// Open the file for input in binary mode.
+	file.open("numbers.dat", ios::in | ios::binary);
+
+	// Read the contents of the file into the array.
+	cout << "Now reading the data back into memory.\n";
+	file.read(reinterpret_cast<char*>(new_numbers), sizeof(new_numbers));
+
+	// Display the contents of the array.
+	for (int count = 0; count < SIZE; count++)
+		cout << new_numbers[count] << " ";
+		 cout << endl;
+
+	// Close the file.
+	file.close();
+	return 0;
 
 }
