519a4528953306de5b680ed32612091be0d7fb60
alphaman9 <zurijb@gmail.com>
2023-02-27 00:17:31 +0000

    Update 16. Customer Accounts.cpp

--- Chapter 12 - Advanced File Operations/16. Customer Accounts.cpp
+++ Chapter 12 - Advanced File Operations/16. Customer Accounts.cpp
@@ -46,6 +46,9 @@ const int ZIP_SIZE = 9;
 const int TELEPHONE_SIZE = 12;
 const int DATE_SIZE = 11;
 
+const string	MONTHS[] = { "January", "February", "March", "April", "May",	"June",
+							"July", "August", "September", "October", "November", "December" };
+
 struct Address {
 	char first_line[ADDRESS_FIRST_LINE_SIZE];
 	char city[CITY_SIZE];
@@ -62,12 +65,19 @@ struct Customer {
 };
 
 int menuInputValidation(int, int, string);
-char* stringInputValidation(string);
+double balanceInputValidation(string);
+void stringInputValidation(char*, const int, string);
+bool isBlank(char*, const int);
 
-string initFile();
+void openFile(fstream &file);
+bool checkFileExists(fstream&, const string);
+
 void showMenu();
 
 void addRecord(fstream &file, const string location);
+
+void dateInput(char[], string);
+bool checkDate(int month, int day, int year, const string MONTHS[]);
 
 int main() {
 
@@ -76,7 +86,7 @@ int main() {
 
 	int choice;
 
-	file_location = initFile();
+	openFile(file);
 
 	showMenu();
 	choice = menuInputValidation(1, 6, "Enter your selection: ");
@@ -87,6 +97,9 @@ int main() {
 			break;
 
 	}
+
+	cout << endl;
+	cout << "Quiting the program..." << endl;
 
 	file.close();
 
@@ -112,47 +125,112 @@ int menuInputValidation(int min, int max, string request) {
 
 }
 
-char* stringInputValidation(string request) {
+double balanceInputValidation(string request) {
 
-	bool isBlank
-	string text;
+	double num;
 
 	cout << request;
 
-	getline(cin, text);
+	while (!(cin >> num) || num < 0) {
+		cout << "ERROR: The starting account cannot be less than $0." << endl;
+		cout << request;
+		cin.ignore();
+	}
 
+	cin.ignore();
 
+	return num;
 
 }
 
+void stringInputValidation(char* text, const int LIMIT, string request) {
 
-string initFile() {
+	cout << request;
 
-	fstream file;
-	string location;
+	while (cin.getline(text, LIMIT) && isBlank(text, LIMIT)) {
+		cout << "ERROR: String cannot be left blank." << endl;
+		cout << request;
+		cin.ignore();
+	}
 
-	cout << "Enter the location of your customer accounts file: ";
-	getline(cin, location);
-
+	cin.ignore();
+	
 	cout << endl;
+}
 
-	file.open(location, ios::out | ios::app);
+bool isBlank(char* text, const int LIMIT) {
 
-	if (!file) {
-		cout << "Error creating the file. Please check your chosen file location, re-run the program and try again." << endl;
-		cout << "Aborting the program..." << endl;
-		exit(EXIT_FAILURE);
+	for (int i = 0; i < LIMIT; i++) {
+
+		if (!isspace(text[i])) {
+			return 0;
+		}
+
 	}
-		
+
+	return 1;
+
+}
+
+bool checkFileExists(fstream& file, const string LOCATION) {
+
+	file.open(LOCATION, ios::in);
+
+	if (file.fail()) {
+		cout << "\"" << LOCATION << "\" does not already exist." << endl;
+		return 0;
+	}
 	else {
-		cout << location << "was successfuly initialzed." << endl;
+		cout << "\"" << LOCATION << "\" was found." << endl;
+		file.close();
+		return 1;
 	}
 
+}
+
+void openFile(fstream &file) {
+
+	string location;
+	long file_size;
+	bool fileExists;
+	bool fileError;
+
+	do {
+
+		fileError = false;
+
+		cout << "Enter the location of your customer accounts file. (If there is not an existing file, one will be created)." << endl;
+		cout << endl;
+		cout << "File location: ";
+		getline(cin, location);
+
+		fileExists = checkFileExists(file, location);
+
+		file.open(location, ios::in | ios::out | ios::binary | ios::app);
+
+		if (!fileExists) {
+			cout << "New inventory file \"" << location << "\" has been created." << endl;
+		}
+
+		else {
+
+			file.seekg(0L, ios::end);
+			file_size = file.tellg();
+
+			if (file_size % sizeof(Customer) != 0 && file_size >= sizeof(Customer)) {
+				file.close();
+				cout << "This file is not a customer accounts file." << endl;
+				cout << endl;
+				fileError = true;
+			}
+
+		}
+
+	} while (fileError);
+			
 	cout << endl;
 
 	file.close();
-
-	return location;
 
 }
 
@@ -179,7 +257,7 @@ void addRecord(fstream& file, const string LOCATION) {
 	file.open(LOCATION, ios::out | ios::app | ios::binary);
 
 	if (file.fail()) {
-		cout << "Error opening " << location << "." < endl;
+		cout << "Error opening " << LOCATION << "." << endl;
 		cout << endl;
 	}
 	else {
@@ -187,8 +265,137 @@ void addRecord(fstream& file, const string LOCATION) {
 		cout << "Enter the details of the new customer record: " << endl;
 		cout << endl;
 
-		cout << "Name: ";
-		cin.getline(new_customer.name, NAME_SIZE);
+		stringInputValidation(new_customer.name, NAME_SIZE, "Name: ");
+		cout << endl;
+
+		cout << "ADDRESS" << endl;
+		cout << "==============" << endl;
+		stringInputValidation(new_customer.address.first_line, ADDRESS_FIRST_LINE_SIZE, "First line: ");
+		stringInputValidation(new_customer.address.city, CITY_SIZE, "City: ");
+		stringInputValidation(new_customer.address.state, STATE_SIZE, "State: ");
+		stringInputValidation(new_customer.address.zip, ZIP_SIZE, "ZIP: ");
+		cout << endl;
+
+		stringInputValidation(new_customer.telephone, TELEPHONE_SIZE, "Telephone Number: ");
+		new_customer.balance = balanceInputValidation("Account Balance: $");
+
+		dateInput(new_customer.date_of_last_payment, "Date of last payment: ");
+
 	}
+
+}
+
+void dateInput(char struct_date[], string request) {
+
+	string date, mm, dd, yyyy;
+	int i = 0, month, day, year;
+	bool valid_date = false;
+
+	do {
+
+		mm = "", dd = "", yyyy = "";
+
+		cout << request;
+		getline(cin, date);
+		cin.clear();
+
+		if (date.length() != DATE_SIZE - 1 || date[2] != '/' || date[5] != '/') {
+
+			cout << "Error: " << date << " must be in the following format: mm/dd/yyyy" << endl;
+
+		}
+
+		else {
+
+			for (int j = 0; j < DATE_SIZE - 1; j++) {
+
+				if (j == 0 || j == 1) {
+					mm += date[j];
+				}
+
+				else if (j == 2 || j == 5) {
+					continue;
+				}
+
+				else if (j == 3 || j == 4) {
+					dd += date[j];
+				}
+
+				else {
+					yyyy += date[j];
+				}
+
+			}
+
+			month = stoi(mm);
+			day = stoi(dd);
+			year = stoi(yyyy);
+
+			valid_date = checkDate(month, day, year, MONTHS);
+
+			if (!valid_date) {
+				cout << date << " is not a valid date. Please try again." << endl;
+			}
+
+		}
+
+	} while (!valid_date);
+
+	for (int i = 0; i < DATE_SIZE - 1; i++) {
+		struct_date[i] = date[i];
+	}
+
+}
+
+bool checkDate(int month, int day, int year, const string MONTHS[]) {
+
+	bool valid_date = true, leap_year = false;
+
+	//LIMITS OF MONTH, DAY AND YEAR ENTRY
+	if (month < 1 || month > 12) {
+		valid_date = false;
+		cout << "Error: Your month entered must be a value between 1 - 12." << endl;
+	}
+
+	if (day < 1 || day > 31) {
+		valid_date = false;
+		cout << "Error: Your day entered must be a value between 1 - 31." << endl;
+	}
+
+	if (year < 1 || year > 9999) {
+		valid_date = false;
+		cout << "Error: Your year entered must be a value between 1 - 9999." << endl;
+	}
+
+	//LEAP YEAR CHECK
+	if (year % 4 == 0) {
+		leap_year = true;
+	}
+
+	//IN-DEPTH DATE CHECK
+	if (month == 4 || month == 6 || month == 9 || month == 11) {
+
+		if (day > 30) {
+			valid_date = false;
+			cout << "Error: " << MONTHS[month - 1] << " cannot have more than 30 days." << endl;
+		}
+
+	}
+
+	if (month == 2) {
+
+		if (leap_year && day > 29) {
+			valid_date = false;
+			cout << "Error: " << MONTHS[month - 1] << " in " << year << " cannot have more than 29 days." << endl;
+		}
+
+		else if (!leap_year && day > 28) {
+			valid_date = false;
+			cout << "Error: " << MONTHS[month - 1] << " in " << year << " cannot have more than 28 days." << endl;
+		}
+
+	}
+
+	return valid_date;
 
 }
