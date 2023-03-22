ba455b987b8f9a95b5309ba571a71eb9bd5f5151
alphaman9 <60569903+alphaman9@users.noreply.github.com>
2023-03-22 16:15:44 +0000

    Update numberArrayClass.cpp

--- Chapter 13 - Introduction To Classes/10 - Number Array Class/numberArrayClass.cpp
+++ Chapter 13 - Introduction To Classes/10 - Number Array Class/numberArrayClass.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-Number::Number(int size) {
+Number::Number(const int size) {
 
 	num = new float[size];
 	cout << "The number array has been created..." << endl;
@@ -16,12 +16,66 @@ Number::~Number() {
 	cout << "Number instances have been destructed...." << endl;
 }
 
-void Number::setNumber(int index, float input) {
+void Number::setNumber(int index) {
+
+	float input;
+
+	cout << "Enter Number #" << index + 1 << ": ";
+
+	while (!(cin >> input)) {
+		cout << "ERROR: Value must be a float. Re-enter your value: ";
+		cin.clear();
+		cin.ignore(1200, '\n');
+	}
+
+	cin.ignore();
+
 	num[index] = input;
+
 }
 
 float Number::getNumber(int index) const {
 	return num[index];
+}
+
+float Number::getHighest(const int SIZE) const {
+
+	float highest = num[0];
+
+	for (int i = 0; i < SIZE; i++) {
+		if (num[i] > highest) {
+			highest = num[i];
+		}
+	}
+
+	return highest;
+
+}
+
+float Number::getLowest(const int SIZE) const {
+
+	float lowest = num[0];
+
+	for (int i = 1; i < SIZE; i++) {
+		if (num[i] < lowest) {
+			lowest = num[i];
+		}
+	}
+
+	return lowest;
+
+}
+
+float Number::getAverage(const int SIZE) const {
+
+	float total = 0;
+
+	for (int i = 0; i < SIZE; i++) {
+		total += num[i];
+	}
+
+	return total / SIZE;
+
 }
 
 
