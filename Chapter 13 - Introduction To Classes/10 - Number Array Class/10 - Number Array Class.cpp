2f2789a94f6b0cb389c02b04bce517a4e21a84dd
alphaman9 <60569903+alphaman9@users.noreply.github.com>
2023-03-22 16:19:32 +0000

    Update 10 - Number Array Class.cpp

--- Chapter 13 - Introduction To Classes/10 - Number Array Class/10 - Number Array Class.cpp
+++ Chapter 13 - Introduction To Classes/10 - Number Array Class/10 - Number Array Class.cpp
@@ -1,3 +1,18 @@
+//10. Number Array Class
+//
+//Design a class that has an array of floating - point numbers.The constructor should
+//accept an integer argument and dynamically allocate the array to hold that many
+//numbers.The destructor should free the memory held by the array.In addition, there
+//should be member functions to perform the following operations :
+//
+//• Store a number in any element of the array
+//• Retrieve a number from any element of the array
+//• Return the highest value stored in the array
+//• Return the lowest value stored in the array
+//• Return the average of all the numbers stored in the array
+//
+//Demonstrate the class in a program.
+
 #include "numberArrayClass.h"
 #include <iostream>
 #include <iomanip>
