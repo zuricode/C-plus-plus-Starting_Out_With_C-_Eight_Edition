91adf5606ff1a764ac3b541d88dea2025ae1384e
alphaman9 <zurijb@gmail.com>
2023-03-27 22:55:08 +0100

    Create coin.cpp

---
+++ Chapter 13 - Introduction To Classes/13 - Tossing Coins for a Dollar/coin.cpp
@@ -0,0 +1,24 @@
+#include "coin.h"
+
+using namespace std;
+
+void Coin::toss() {
+
+	int num;
+
+	num = rand() % 2;
+
+	if (num) {
+		sideUp = "HEADS";
+	}
+	else {
+		sideUp = "TAILS";
+	}
+
+}
+
+Coin::Coin() {
+	toss();
+}
+
+
