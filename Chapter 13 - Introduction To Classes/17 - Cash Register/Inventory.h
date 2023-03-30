b808633f58c4e841836a8c6b8da49ce9056a99f3
alphaman9 <60569903+alphaman9@users.noreply.github.com>
2023-03-30 13:59:33 +0100

    Create Inventory.h

---
+++ Chapter 13 - Introduction To Classes/17 - Cash Register/Inventory.h
@@ -0,0 +1,41 @@
+#ifndef INVENTORY_H
+#define INVENTORY_H
+#include<string>
+
+using std::string;
+
+class Inventory {
+
+private:
+
+	string name;
+	int itemNumber;
+	double cost;
+	int quantity;
+	double totalCost;
+
+public:
+
+	Inventory();
+	Inventory(int, double, int);
+
+	void setName(string n);
+	void setItemNumber(int);
+	void setCost(double);
+	void setQuantity(int);
+	void setTotalCost();
+
+	string getName() const { return name; }
+	int getItemNumber() const;
+	double getCost() const;
+	int getQuantity() const;
+	double getTotalCost() const;
+
+	int numValidation(int);
+	double numValidation(double);
+
+	~Inventory();
+
+};
+
+#endif
