2fb8f36d593ce493fac17f6f41c03fc192ff8769
alphaman9 <60569903+alphaman9@users.noreply.github.com>
2023-03-30 13:59:16 +0100

    Update Inventory.h

--- Chapter 13 - Introduction To Classes/06 - Inventory Class/Inventory.h
+++ Chapter 13 - Introduction To Classes/06 - Inventory Class/Inventory.h
@@ -1,10 +1,14 @@
 #ifndef INVENTORY_H
 #define INVENTORY_H
+#include<string>
+
+using std::string;
 
 class Inventory {
 
 private:
 
+	string name;
 	int itemNumber;
 	double cost;
 	int quantity;
@@ -13,13 +17,15 @@ private:
 public:
 
 	Inventory();
-	Inventory(int, double, int);
+	Inventory(string, int, double, int);
 
+	void setName(string n);
 	void setItemNumber(int);	
 	void setCost(double);
 	void setQuantity(int);
 	void setTotalCost();
 
+	string getName() const { return name; }
 	int getItemNumber() const;
 	double getCost() const;
 	int getQuantity() const;
@@ -27,6 +33,9 @@ public:
 
 	int numValidation(int);
 	double numValidation(double);
+	string stringValidation();
+
+	~Inventory();
 
 };
 
