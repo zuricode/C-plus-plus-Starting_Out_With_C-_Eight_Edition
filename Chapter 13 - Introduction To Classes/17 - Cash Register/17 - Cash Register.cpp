1df9277033694da3b13ccda5a03dc54aaa394a84
alphaman9 <zurijb@gmail.com>
2023-03-31 20:32:00 +0100

    Update 17 - Cash Register.cpp

--- Chapter 13 - Introduction To Classes/17 - Cash Register/17 - Cash Register.cpp
+++ Chapter 13 - Introduction To Classes/17 - Cash Register/17 - Cash Register.cpp
@@ -23,24 +23,65 @@
 #include<iostream>
 #include<iomanip>
 #include<string>
+#include<vector>
 
 using namespace std;
+
+void displayInventory(vector<Inventory>);
 
 int main() {
 
 	CashRegister store;
-	Inventory item;
+	vector<Inventory> stockroom;
+	int item_choice;
+	int item_quantity;
 
 	cout << "13-17 CASH REGISTER PROGRAM\n";
 	cout << "==================================\n";
 	cout << "\n";
 
-	item = store.askForItem();
+	Inventory jerk_sauce("Walkerswood Jerk Sauce Jar (250g)", 34543, 2.49, 20);
+	Inventory toilet_tissue("48 Rolls of Extra Soft Toilet Tissue", 45430, 12.99, 5);
+	Inventory spring_water("1L Spring Water", 00533, 1.29, 50);
+	Inventory lamb_chops("1kg Organic Lamb Chops", 88001, 11.49, 12);
 
-	store.getCost(item);
+	stockroom.push_back(jerk_sauce);
+	stockroom.push_back(toilet_tissue);
+	stockroom.push_back(spring_water);
+	stockroom.push_back(lamb_chops);
 
-	store.addProfit(item);
+	displayInventory(stockroom);
 
-	store.showTotals(item);
+	item_choice = store.askForItem(stockroom);
+	item_quantity = store.askForQuantity(stockroom[item_choice]);
+
+	store.getCost(stockroom[item_choice]);
+	store.addProfit(stockroom[item_choice]);
+	store.showTotals(stockroom[item_choice], item_quantity);
+
+	store.deductQuantityFromStock(stockroom[item_choice], item_quantity);
+
+	displayInventory(stockroom);
+
+
+}
+
+void displayInventory(vector<Inventory> stockroom) {
+
+	const int DIV = 80;
+
+	cout << "INVENTORY STOCKROOM\n";
+	cout << right << setfill('=') << setw(20) << "\n";
+	cout << left << setfill(' ') << "\n";
+	cout << setw(10) << "#" << setw(40) << "ITEM NAME" << setw(15) << "ITEM NUMBER" << setw(10) << "COST" << "QUANTITY\n";
+	cout << right << setfill('-') << setw(DIV) << "\n";
+	cout << left << setfill(' ');
+	
+	for (int i = 0; i < stockroom.size(); i++) {
+		cout << "[" << i + 1 << setw(8) << "]" << setw(40) << stockroom[i].getName() << setw(15) << setprecision(0) << stockroom[i].getItemNumber() << "$" << fixed << setprecision(2) << setw(9) << stockroom[i].getCost() << setprecision(0) << stockroom[i].getQuantity() << "\n";
+	}
+
+	cout << right << setfill('-') << setw(DIV) << "\n";
+	cout << "\n";
 
 }
