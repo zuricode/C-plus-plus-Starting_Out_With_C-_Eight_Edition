0720efa671ae19f81e35a242c7a8b1a3f6e96b18
alphaman9 <zurijb@gmail.com>
2023-03-31 20:32:04 +0100

    Update CashRegister.h

--- Chapter 13 - Introduction To Classes/17 - Cash Register/CashRegister.h
+++ Chapter 13 - Introduction To Classes/17 - Cash Register/CashRegister.h
@@ -1,19 +1,25 @@
 #ifndef CASHREGISTER_H
 #define CASHREGISTER_H
 #include "inventory.h"
+#include<vector>
+
+using std::vector;
 
 class CashRegister {	
 
 public:
 
-	Inventory askForItem();
+	int askForItem(vector<Inventory>);
+	int askForQuantity(Inventory);
 
 	void getCost(Inventory);
 	void addProfit(Inventory &);
-	double getSubTotal(Inventory);
-	double getSalesTax(Inventory);
-	void showTotals(Inventory);
-	double getGrandTotal(Inventory);
+	double getSubTotal(Inventory, int);
+	double getSalesTax(Inventory, int);
+	void showTotals(Inventory, int);
+	double getGrandTotal(Inventory, int);
+
+	void deductQuantityFromStock(Inventory &, int);
 
 };
 
