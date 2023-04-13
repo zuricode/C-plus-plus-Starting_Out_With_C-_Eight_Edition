9195c67373ebd6160531aedc32723a47c5906c56
alphaman9 <zurijb@gmail.com>
2023-04-14 00:07:53 +0100

    Create 20. Patient Fees.cpp

---
+++ Chapter 13 - Introduction To Classes/20 - Patient Fees/20. Patient Fees.cpp
@@ -0,0 +1,64 @@
+//20. Patient Fees
+//
+//1. This program should be designed and written by a team of students.Here are some
+//suggestions :
+//
+//– One or more students may work on a single class.
+//– The requirements of the program should be analyzed so each student is given
+//about the same workload.
+//– The parameters and return types of each function and class member function
+//should be decided in advance.
+//– The program will be best implemented as a multi - file program.
+//
+//2. You are to write a program that computes a patient’s bill for a hospital stay.The
+//
+//The PatientAccount class
+//The Surgery class
+//The Pharmacy class
+//The main program
+//
+//– The PatientAccount class will keep a total of the patient’s charges.It will also
+//keep track of the number of days spent in the hospital.The group must decide
+//on the hospital’s daily rate.
+//– The Surgery class will have stored within it the charges for at least five types
+//of surgery.It can update the charges variable of the PatientAccount class.
+//– The Pharmacy class will have stored within it the price of at least five types of
+//medication.It can update the charges variable of the PatientAccount class.
+//– The student who designs the main program will design a menu that allows the
+//user to enter a type of surgeryand a type of medication, and check the patient
+//out of the hospital.When the patient checks out, the total charges should be
+//displayed.
+
+#include "Patient.h"
+#include "Surgery.h"
+#include "Pharmacy.h"
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+void showMenu();
+
+int main() {
+
+	cout << "13-20 PATIENT DATABASE PROGRAM\n";
+	cout << "===================================\n";
+	cout << "\n";
+
+
+
+
+}
+
+void showMenu() {
+
+	cout << "Menu\n";
+	cout << "---------\n";
+	cout << "\n";
+
+	cout << "\t[1] - ADD NEW PATIENT\n";
+	cout << ""
+
+
+}
