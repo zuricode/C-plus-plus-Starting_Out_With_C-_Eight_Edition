ce798b58f52e4dd9cf467da4f3e211f2410282c1
alphaman9 <60569903+alphaman9@users.noreply.github.com>
2023-04-15 21:35:33 +0100

    Update Surgery.h

--- Chapter 13 - Introduction To Classes/20 - Patient Fees/Surgery.h
+++ Chapter 13 - Introduction To Classes/20 - Patient Fees/Surgery.h
@@ -3,22 +3,24 @@
 #include <string>
 #include "Patient.h"
 
-const int TYPES_OF_SURGERY = 5;
-
 class Surgery {
 
 private:
 
 
-	string surgery_type[TYPES_OF_SURGERY]{ "Appendectomy", "Colostomy", "MRI Scan", "X-Ray", "Hip Replacement" };
-	double charges[TYPES_OF_SURGERY]{ 1500.00, 4000.00, 750.00, 1000.00, 9425.00 };
-	int surgery_days[TYPES_OF_SURGERY]{ 4, 5, 1, 1, 14 };
+	const string type[TYPES_OF_SURGERY]{ "Appendectomy", "Colostomy", "MRI Scan", "X-Ray", "Hip Replacement" };
+	const double charge[TYPES_OF_SURGERY]{ 1500.00, 4000.00, 750.00, 1000.00, 9425.00 };
+	const int timeframe_days[TYPES_OF_SURGERY]{ 4, 5, 1, 1, 14 };
 
 public:
 	Surgery() {}
 	~Surgery() {}
 
 	void addCharge(Patient&, double);
+
+	string getType() { return type; } 
+	double getCharge() { return charge; } 
+	int getTimeframeDays() { return timeframe_days; }
 
 };
 
