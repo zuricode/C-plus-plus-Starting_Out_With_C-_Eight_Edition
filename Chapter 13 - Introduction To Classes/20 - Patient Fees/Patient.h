f09036b0b300edd3b3a1da2f93bd5a9c72a627ea
alphaman9 <60569903+alphaman9@users.noreply.github.com>
2023-04-16 13:45:32 +0100

    Update Patient.h

--- Chapter 13 - Introduction To Classes/20 - Patient Fees/Patient.h
+++ Chapter 13 - Introduction To Classes/20 - Patient Fees/Patient.h
@@ -1,14 +1,17 @@
 #ifndef PATIENT_H
 #define PATIENT_H
 #include <string>
+#include <vector>
 
 using std::string;
+using std::vector;
 
 class Patient {
 
 private:
 	string full_name;
 	double total_charges;
+	vector<string> list_of_operations;
 	int total_operations;
 	int days_in_hospital;
 
@@ -19,11 +22,17 @@ public:
 	void setName(string s) { full_name = s; }
 	string getName() { return full_name; }
 
-	void addOperation() { total_operations++; }
+	void addCharge(double charge) { total_charges += charge; }
+	double getTotalCharges() { return total_charges; }
+
+	void addOperationTitle(string title) { list_of_operations.push_back(title); }
+	void getAllOperations();
+
+	void addDaysInHospital(int days) { days_in_hospital += days; }
+	int getDaysInHospital() { return days_in_hospital;  }
+
+	void addOperationAmount() { total_operations++; }
 	int getTotalOperations() { return total_operations; }
-
-	void addCharge(double);
-
 };
 
 #endif
