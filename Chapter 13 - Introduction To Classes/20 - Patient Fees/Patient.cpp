e4bea0347af5a610af02b5f3ce1a7a758eaa85c7
alphaman9 <zurijb@gmail.com>
2023-04-20 15:03:24 +0100

    Update Patient.cpp

--- Chapter 13 - Introduction To Classes/20 - Patient Fees/Patient.cpp
+++ Chapter 13 - Introduction To Classes/20 - Patient Fees/Patient.cpp
@@ -1,20 +1,8 @@
 #include "Patient.h"
-#include <iostream>
-#include <string>
-#include <vector>
 
-using namespace std;
+void Patient::addCharge(double charge) {
 
-void Patient::getAllOperations() {
-
-	for (int i = 0; i < list_of_operations.size(); i++) {
-
-		if (i != 0) {
-			cout << ", ";
-		}
-		
-		cout << list_of_operations[i];
-
-	}
+	total_charges += charge;
 
 }
+
