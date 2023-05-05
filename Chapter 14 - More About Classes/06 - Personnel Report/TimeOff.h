66ba5a150c1d219ba9a0b59837c99da6d1b0d859
alphaman9 <60569903+alphaman9@users.noreply.github.com>
2023-05-05 15:08:51 +0100

    Create TimeOff.h

---
+++ Chapter 14 - More About Classes/06 - Personnel Report/TimeOff.h
@@ -0,0 +1,51 @@
+#ifndef	TIMEOFF_H
+#define TIMEOFF_H
+#include "NumDays.h"
+#include<string>
+
+using std::string;
+
+class TimeOff {
+
+private:
+
+	string name;
+	int id_number;
+	NumDays maxSickDays;
+	NumDays sickTaken;
+	NumDays maxVacation;
+	NumDays vacTaken;
+	NumDays maxUnpaid;
+	NumDays unpaidTaken;
+
+public:
+
+	TimeOff();
+	TimeOff(string, int, int, int, int, int, int, int);
+ 
+	void setName(string);
+	void setIdNumber(int);
+	void setMaxSickDays(int hours) { maxSickDays.setHours(hours); }
+	void setSickTaken(int hours) { sickTaken.setHours(hours); }
+	void setMaxVacation(int hours) { maxVacation.setHours(hours); }
+	void setVacTaken(int hours) { vacTaken.setHours(hours); }
+	void setMaxUnpaid(int hours) { maxUnpaid.setHours(hours); }
+	void setUnpaidTaken(int hours) { unpaidTaken.setHours(hours); }
+
+	void maxVacationValidation();
+
+	string getName() { return name; }
+	int getIdNumber() { return id_number; }
+	double getMaxSickDays() { return maxSickDays.getDays(); }
+	double getSickTaken() { return sickTaken.getDays(); }
+	double getMaxVacation() { return maxVacation.getDays(); }
+	double getVacTaken() { return vacTaken.getDays(); }
+	double getMaxUnpaid() { return maxUnpaid.getDays(); }
+	double getUnpaidTaken() { return unpaidTaken.getDays(); }
+
+	double getVacDays();
+	double getSickDays();
+
+};
+
+#endif
