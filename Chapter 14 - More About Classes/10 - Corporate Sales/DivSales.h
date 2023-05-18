63b631dabb1c6ac8f238313403adbb24e4dde196
alphaman9 <60569903+alphaman9@users.noreply.github.com>
2023-05-18 18:04:20 +0100

    Update DivSales.h

--- Chapter 14 - More About Classes/10 - Corporate Sales/DivSales.h
+++ Chapter 14 - More About Classes/10 - Corporate Sales/DivSales.h
@@ -1,11 +1,13 @@
 #ifndef DIVSALES_H
 #define DIVSALES_H
 
+const int NUMBER_OF_QUARTERS = 4;
+
 class DivSales {
 
 private:
 
-	double sales_data[4];
+	double sales_data[NUMBER_OF_QUARTERS];
 	static double total_annual_corp_sales;
 
 public:
@@ -15,7 +17,9 @@ public:
 
 	void addSalesData(const double, const double, const double, const double);
 	void enterSalesData();
+
 	double getSalesFromQuarter(int) const;
+	double getTotalDivSales() const;
 
 	double getTotalAnnualCorpSales() const;
 
