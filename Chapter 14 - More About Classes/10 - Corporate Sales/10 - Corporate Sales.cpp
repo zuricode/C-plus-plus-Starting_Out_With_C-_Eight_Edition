38e69401b142a7a3433c02ac89af3abd5f091fde
alphaman9 <60569903+alphaman9@users.noreply.github.com>
2023-05-18 14:06:17 +0100

    Update 10 - Corporate Sales.cpp

--- Chapter 14 - More About Classes/10 - Corporate Sales/10 - Corporate Sales.cpp
+++ Chapter 14 - More About Classes/10 - Corporate Sales/10 - Corporate Sales.cpp
@@ -4,15 +4,75 @@
 
 using namespace std;
 
+void displayAllQuarterlyEarnings(DivSales [], const int, const int);
+void enterData(DivSales [], const int);
+void displayCompanyEarningsTable(const DivSales, const int);
+
 int main() {
 
-	const int NUMBER_OF_DIVISION = 6;
+	const int NUMBER_OF_DIVISIONS = 6;
 
-	DivSales company[NUMBER_OF_DIVISION];
+	DivSales company[NUMBER_OF_DIVISIONS];
 
-	company[0].addSalesData(3453.45, 456.34, 45323.55, 34645);
+	enterData(company, NUMBER_OF_DIVISIONS);
+
+	displayAllQuarterlyEarnings(company, NUMBER_OF_DIVISIONS, 0);
 
 	cout << "Total annual sales for all divisions: $" << fixed << setprecision(2) << company[0].getTotalAnnualCorpSales() << "\n";
 	cout << "\n";
+
+}
+
+void enterData(DivSales d[], const int NUMBER_OF_DIVISIONS) {
+
+	cout << "DATA ENTRY FOR COMPANY\n";
+	cout << "======================\n";
+	cout << "\n";
+
+	for (int i = 0; i < NUMBER_OF_DIVISIONS; i++) {
+
+		cout << "Division #" << i + 1 << "\n";
+
+		if (i != 2 && i != 5) {
+			d[i].enterSalesData();
+		}
+		else {
+			cout << "NOTE: Division #" << i + 1 << " was inactive this year.\n";
+			cout << "\n";
+			d[i].addSalesData(0, 0, 0, 0);
+		}
+
+		cout << "\n";
+	}
+
+}
+
+
+void displayAllQuarterlyEarnings(DivSales d[], const int NUMBER_OF_DIVISIONS, const int I) {
+
+	cout << fixed << setprecision(2);
+
+	for (int i = 0; i < NUMBER_OF_DIVISIONS; i++) {
+
+		cout << "DIVISION #" << i + 1 << "\n";
+		cout << "====================\n";
+		cout << "\n";
+
+		for (int j = 0; j < 4; j++) {
+
+			cout << "$" << d[i].getSalesFromQuarter(j) << " was earned in Q" << j + 1 << "\n";
+
+		}
+
+		cout << "\n";
+
+	}
+
+}
+
+void displayCompanyEarningsTable(const DivSales D, const int NUMBER_OF_DIVISIONS) {
+
+	cout << "COMPANY EARNINGS REPORT\n";
+	cout << left << setfill('=') << setw(70) <<
 
 }
