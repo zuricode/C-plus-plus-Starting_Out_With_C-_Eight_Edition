65f18d350f83a8fa9d4afee7ac3968fff1067166
alphaman9 <60569903+alphaman9@users.noreply.github.com>
2023-05-22 11:55:19 +0100

    Create 12 - LandTract Demonstration.cpp

---
+++ Chapter 14 - More About Classes/12 - LandTract Class/12 - LandTract Demonstration.cpp
@@ -0,0 +1,53 @@
+#include "LandTract.h"
+
+using namespace std;
+
+void enterLandData(LandTract &);
+
+int main() {
+
+	LandTract land_1;
+	LandTract land_2;
+
+	cout << "-------------------------------------\n";
+	cout << "14-12 LANDTRACK PROGRAM DEMONSTRATION\n";
+	cout << "-------------------------------------\n";
+
+	cout << "\n";
+
+	cout << "LAND #1\n";
+	cout << "--------\n";
+
+	enterLandData(land_1);
+
+	cout << "LAND #2\n";
+	cout << "--------\n";
+
+	enterLandData(land_2);
+
+	cout << "Land #1 Area: " << land_1.getArea() << "\n";
+	cout << "Land #2 Area: " << land_2.getArea() << "\n";
+
+	if (land_1.getArea() == land_2.getArea()) {
+		cout << "Both plots of land are the same size.\n";
+	}
+	else {
+		cout << "The plots of land are not equal in size.\n";
+	}
+
+}
+
+void enterLandData(LandTract& l) {
+
+	cout << "Enter the length of your piece of land...\n";
+	l.setLength();
+
+	cout << "\n";
+
+	cout << "Enter the width of your piece of land...\n";
+	l.setWidth();
+
+	cout << "\n";
+
+
+}
