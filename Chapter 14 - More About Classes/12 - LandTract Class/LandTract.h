f1f74ab5f5368f90efed0f20ed67a6e9b0aa3564
alphaman9 <60569903+alphaman9@users.noreply.github.com>
2023-05-22 11:55:33 +0100

    Create LandTract.h

---
+++ Chapter 14 - More About Classes/12 - LandTract Class/LandTract.h
@@ -0,0 +1,25 @@
+#ifndef LANDTRACT_H
+#define LANDTRACT_H
+
+#include "FeetInches.h"
+
+class LandTract {
+
+private:
+	FeetInches length;
+	FeetInches width;
+
+public:
+
+	void setLength();
+	void setWidth();
+
+	FeetInches getLength() const { return length; }
+	FeetInches getWidth() const { return width;  }
+
+	FeetInches getArea();
+
+
+};
+
+#endif
