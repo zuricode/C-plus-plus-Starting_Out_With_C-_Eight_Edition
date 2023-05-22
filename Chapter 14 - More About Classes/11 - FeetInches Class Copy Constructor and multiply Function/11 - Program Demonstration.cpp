3e2d247db121f5911f926a22d4029d99c15b2ab5
alphaman9 <60569903+alphaman9@users.noreply.github.com>
2023-05-22 11:15:46 +0100

    Create 11 - Program Demonstration.cpp

---
+++ Chapter 14 - More About Classes/11 - FeetInches Class Copy Constructor and multiply Function/11 - Program Demonstration.cpp
@@ -0,0 +1,29 @@
+#include "FeetInches.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	FeetInches person_height(15, 4);
+	FeetInches person_clone(person_height);
+	FeetInches new_person;
+
+	cout << "-----------------------------\n";
+	cout << "14-11 FeetInches Copy Constructor and Multiply Function Demonstration\n";
+	cout << "-----------------------------\n";
+	cout << "\n";
+
+	cout << "Original Person: " << person_height << "\n";
+	cout << "Clone Person: " << person_clone << "\n";
+
+	new_person = person_height.multiply(person_clone);
+
+	cout << "\n";
+
+	cout << "The height of NEW PERSON will be (ORIGINAL PERSON * CLONE PERSON)...\n";
+	cout << "New person: " << new_person << "\n";
+
+
+
+}
