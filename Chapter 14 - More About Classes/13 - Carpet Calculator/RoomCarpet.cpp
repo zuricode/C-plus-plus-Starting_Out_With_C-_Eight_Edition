99933839fd705bf3c64761557029556c0431d4ec
alphaman9 <zurijb@gmail.com>
2023-05-27 14:31:01 +0100

    Update RoomCarpet.cpp

--- Chapter 14 - More About Classes/13 - Carpet Calculator/RoomCarpet.cpp
+++ Chapter 14 - More About Classes/13 - Carpet Calculator/RoomCarpet.cpp
@@ -2,12 +2,45 @@
 #include "RoomDimension.h"
 #include "FeetInches.h"
 
-double RoomCarpet::calculateCost() const {
+double RoomCarpet::calculateCost() {
 
 	double area;
 	
 	area = room.getAreaSqFt();
 
 	return area * cost_per_sq_ft;
+
+}
+
+double RoomCarpet::getRoomArea() {
+
+	return room.getAreaSqFt();
+
+}
+
+
+void RoomCarpet::setRoomArea() {
+
+	room.setLengthAndWidth();
+
+}
+
+void RoomCarpet::setCost() {
+
+	double num;
+
+	cout << "Enter the cost of a square foot of your chosen carpet: $";
+
+	while (!(cin >> num) || num < 0) {
+		cout << "ERROR: Cost cannot be a negative value.\n";
+		cout << "\n";
+		cout << "Cost: $";
+		cin >> num;
+	}
+	
+	cost_per_sq_ft = num;
+
+	cout << "\n";
+
 
 }
