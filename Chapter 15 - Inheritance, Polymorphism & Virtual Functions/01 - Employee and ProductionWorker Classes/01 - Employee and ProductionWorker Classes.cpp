500be407f8bdfcf922526c54411492f8e27c57b2
alphaman9 <zurijb@gmail.com>
2023-06-06 13:48:22 +0100

    Update 01 - Employee and ProductionWorker Classes.cpp

--- Chapter 15 - Inheritance, Polymorphism & Virtual Functions/01 - Employee and ProductionWorker Classes/01 - Employee and ProductionWorker Classes.cpp
+++ Chapter 15 - Inheritance, Polymorphism & Virtual Functions/01 - Employee and ProductionWorker Classes/01 - Employee and ProductionWorker Classes.cpp
@@ -4,9 +4,9 @@ using namespace std;
 
 int main() {
 
-	cout << "=============================================\n";
-	cout << "01 - Employee and ProductionWorker Classes\n";
-	cout << "=============================================\n";
+	cout << "==================================================\n";
+	cout << "01 - Employee and ProductionWorker Classes Program\n";
+	cout << "==================================================\n";
 	cout << "\n";
 
 	ProductionWorker worker(1, 14.50, "Zaire Sarr", 56332, "06/08/2019");
