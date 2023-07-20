66049d3c1ac7a107d9751d9af784abe0250010f5
alphaman9 <60569903+alphaman9@users.noreply.github.com>
2023-07-20 16:36:36 +0100

    12 - Specialized Templates

---
+++ Chapter 16 - Exceptions & Templates/12 - Specialized Templates/12  -Specialized Templates Program.cpp
@@ -0,0 +1,21 @@
+//12. Specialized Templates
+//
+//In this chapter, the section Specialized Templates within Section 16.4 describes
+//how to design templates that are specialized for one particular data type.The
+//section introduces a method for specializing a version of the SimpleVector class
+//template so it will work with strings.Complete the specialization for both the
+//SimpleVector and SearchableVector templates.Demonstrate them with a simple
+//driver program.
+
+#include "SimpleVector.h"
+
+int main() {
+
+	cout << "=====================================\n";
+	cout << "16 - 12 SPECIALIZED TEMPLATES PROGRAM\n";
+	cout << "=====================================\n";
+	cout << "\n";
+
+
+
+}
