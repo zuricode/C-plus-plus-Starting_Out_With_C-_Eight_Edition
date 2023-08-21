cb2996855b3958f0f725b139dd5e6ec55f8c2735
zuricode <zurijb@gmail.com>
2023-08-21 18:33:14 +0100

    09 - Gas Prices Program

---
+++ Chapter 17 - Introduction to the Standard Template Library/09 - Gas Prices/09 - Gas Prices Program.cpp
@@ -0,0 +1,52 @@
+//9. Gas Prices
+//
+//In the student sample program files for this chapter, you will find a text file
+//named GasPrices.txt. The file contains the weekly average prices for a gallon of gas in the U.S,
+//beginning on April 5, 1993 and ending on August 26, 2013. Figure 17-10 shows an
+//example of the first few lines of the file's contents.
+//
+//Each line in the file contains the average price for a gallon of gas on a specific
+//date. Each line is formatted in the following way:
+//
+//	MM-DD-YYYY:Price
+//
+//MM is the two-digit month, DD is the two-digit day, and YYYY is the four-digit
+//year. Price is the average price per gallon of gas on the specified date.
+//
+//For this assignment, you are write one or more programs that read the contents
+//of the file and perform the following calculations:
+//
+//Average Price Per Year: Calculate the average price of gas per year, for each
+//year in the file. (The file's data starts in April of 1993, and it ends in August
+//2013. Use the data that is present for the years 1993 and 2013.)
+//
+//Average Price Per Month: Calculate the average price for each month in the file.
+//
+//Highest and Lowest Prices Per Year: For each year in the file, determine the date
+//and amount for the lowest price, and the highest price.
+//
+//List of Prices, Lowest to Highest: Generate a text file that lists the dates and
+//prices, sorted from the lowest price to the highest.
+//
+//List of Prices, Highest to Lowest: Generate a text file that lists the dates and
+//prices, sorted from the highest price to the lowest.
+
+#include "C:\\Users\\Admin\\source\\repos\\Project2\\Project2\\C-plus-plus-Starting_Out_With_C-_Eight_Edition\\Chapter 17 - Introduction to the Standard Template Library\\09 - Gas Prices\\GasPrices.txt"
+#include<iostream>
+#include<fstream>
+
+using namespace std;
+
+int main() {
+
+	cout << "========================\n";
+	cout << "17-09 GAS PRICES PROGRAM\n";
+	cout << "========================\n";
+	cout << "\n";
+
+
+
+	cout << "Ending the program..\n";
+	cout << "\n";
+
+}
