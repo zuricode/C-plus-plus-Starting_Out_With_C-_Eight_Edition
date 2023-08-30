f36ea2d3fec4ccacacf922a35ebb295fb5668238
zuricode <zurijb@gmail.com>
2023-08-30 23:41:11 +0100

    01 - Your Own Linked List

---
+++ Chapter 18 - Linked Lists/01 - Your Own Linked List/01 - Your Own Linked List.cpp
@@ -0,0 +1,30 @@
+//1. Your Own Linked List
+//
+//Design your own linked list class to hold a series of integers. The class should have
+//member fundtions for appending, inserting and deleting nodes. Don't forget to a add a
+//destructor that destorys the list. Demonstrate the class with a driver program.
+
+#include "NumberList.h"
+
+int main() {
+
+	cout << "==========================\n";
+	cout << "18-01 YOUR OWN LINKED LIST\n";
+	cout << "==========================\n";
+	cout << "\n";
+
+	NumberList list;
+
+	list.appendNode(4);
+	list.appendNode(8);
+	list.appendNode(16);
+	list.appendNode(32);
+
+	list.displayList();
+
+	list.deleteNode(16);
+	list.deleteNode(4);
+
+	list.displayList();
+
+}
---
+++ Chapter 18 - Linked Lists/01 - Your Own Linked List/NumberList.cpp
@@ -0,0 +1,118 @@
+#include "NumberList.h"
+
+NumberList::~NumberList() {
+
+	ListNode* nodePtr;
+	ListNode* nextNode;
+	int num;
+
+	nodePtr = head;
+
+	while (nodePtr) {
+		nextNode = nodePtr->next;
+		num = nodePtr->value;
+		delete nodePtr;
+		cout << "Node with the value \"" << num << "\" was deleted.\n";
+		cout << "\n";
+		nodePtr = nextNode;
+	}
+
+	head = nullptr;
+
+	cout << "Destructor activated. Linked list has been deleted.\n";
+	cout << "\n";
+
+}
+
+void NumberList::displayList() {
+
+	ListNode* nodePtr;
+
+	cout << "List Values\n";
+	cout << "-----------\n";
+
+	nodePtr = head;
+
+	while (nodePtr) {
+		cout << nodePtr->value << " ";
+		nodePtr = nodePtr->next;
+	}
+
+	cout << "\n";
+	cout << "-----------\n";
+	cout << "\n";
+
+}
+
+void NumberList::appendNode(const int NUM) {
+
+	ListNode* nodePtr;
+
+	ListNode* newNode = new ListNode;
+	newNode->value = NUM;
+	newNode->next = nullptr;
+
+	if (!head) {
+		head = newNode;
+	}
+
+	else {
+
+		nodePtr = head;
+
+		while (nodePtr->next) {
+			nodePtr = nodePtr->next;
+		}
+
+		nodePtr->next = newNode;	
+
+	}	
+
+	cout << "A new node was added with the value \"" << NUM << "\".\n";
+	cout << "\n";
+}
+
+void insertNode(const int NUM) {
+
+	ListNode* nodePtr;
+	ListNode* previousNode;
+
+	ListNode* newNode = new ListNode;
+	newNode->value = NUM;
+	newNode->next = nullptr;
+
+}
+
+void NumberList::deleteNode(const int NUM) {
+
+	ListNode* nodePtr;
+	ListNode* newNode;
+	ListNode* previousNode = nullptr;
+
+	if (head->value == NUM) {
+		newNode = head->next;
+		delete head;
+		cout << "Node with the value \"" << NUM << "\" was deleted.\n";
+		cout << "\n";
+		head = newNode;
+	}
+	else {
+
+		nodePtr = head;
+
+		while (nodePtr->value != NUM) {
+
+			previousNode = nodePtr;
+			nodePtr = nodePtr->next;
+
+		}
+
+		newNode = nodePtr->next;
+		delete nodePtr;
+		cout << "Node with the value \"" << NUM << "\" was deleted.\n";
+		cout << "\n";
+		previousNode->next = newNode;
+
+	}
+
+}
