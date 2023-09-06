cd5a0018e5e3ff70c087b4d75699aa1dfa65d46a
zuricode <zurijb@gmail.com>
2023-09-06 23:48:40 +0100

    10 - Payroll Modification

---
+++ Chapter 18 - Linked Lists/10 - Payroll Modification/10 - Payroll Modification Program.cpp
@@ -0,0 +1,33 @@
+//07-09. Payroll
+//
+//Write a program that uses the following arrays :
+//
+//• empId : an array of seven long integers to hold employee identification numbers.The
+//array should be initialized with the following numbers :
+//5658845 4520125 7895122 8777541
+//8451277 1302850 7580489
+//• hours : an array of seven integers to hold the number of hours worked by each
+//employee
+//• payRate : an array of seven double s to hold each employee’s hourly pay rate
+//• wages : an array of seven double s to hold each employee’s gross wages
+//
+//The program should relate the data in each array through the subscripts.For example,
+//the number in element 0 of the hours array should be the number of hours worked by
+
+//18-10. Payroll Modification
+//
+//Modify Programming Challenge 9 in Chapter 7 (Payroll) to use three linked lists instead of
+//three arrays to hold the employee IDs, hours worked, and wages. When the program starts,
+//it should ask the user to enter the employee IDs. There should be no limit on the number of
+//IDs the user can enter.
+
+int main() {
+
+	cout << "==================================\n";
+	cout << "18-10 PAYROLL MODIFICATION PROGRAM\n";
+	cout << "==================================\n";
+	cout << "\n";
+
+
+
+}
