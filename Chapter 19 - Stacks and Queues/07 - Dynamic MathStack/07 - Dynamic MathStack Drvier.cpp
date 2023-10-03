42c7134530df587d8c06e2d2337d79d035b25df3
alphaman9 <zurijb@gmail.com>
2023-10-03 16:49:24 +0100

    07 - Dynamic MathStack

---
+++ Chapter 19 - Stacks and Queues/07 - Dynamic MathStack/07 - Dynamic MathStack Drvier.cpp
@@ -0,0 +1,8 @@
+7. Dynamic MathStack
+
+The MathStack class shown in this vhapter has only two member functions: add and sub. Write the following
+additional member functions:
+
+Function	Description
+mult		Pops the top two values off the stack, multiples them  and pushes their product onto the stack.
+div			Pops the top two values off the stack, divides the second value by the first and pushes the
