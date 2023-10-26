184881b453cd0a9e6bb213f2e664c977d9fddf7e
alphaman9 <zurijb@gmail.com>
2023-10-26 15:35:37 +0100

    03 - QuickSort Template

---
+++ Chapter 20 - Recursion/03 - QuickSort Template/03 - QuickSort Template Driver.cpp
@@ -0,0 +1,15 @@
+//3. QuickSort Template
+//
+//Create a template version of the QuickSort algorithm that will work with any data type.
+//Demonstrate the template with a driver function.
+
+int main() {
+
+	cout << "===============================\n";
+	cout << "20-03 QUICKSORT TEMPLATE DRIVER\n";
+	cout << "===============================\n";
+	cout << "\n";
+
+
+
+}
