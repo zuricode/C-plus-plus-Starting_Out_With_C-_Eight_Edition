307dbf426798ce242bb21fc1ba9b42cd7efd58ac
alphaman9 <zurijb@gmail.com>
2023-10-28 16:54:16 +0100

    05 - Recursive Multiplication

---
+++ Chapter 20 - Recursion/05 - Recursive Multiplication/05 - Recursive Multiplication Driver.cpp
@@ -0,0 +1,58 @@
+//5. Recursive Multiplication
+//
+//Write a recursive function that aceepts two arguments into the parameters x and y. The function
+//should retun the value of x times y. Remember, multiplication can be performed as repeated 
+//addition:
+//
+//7 * 4 = 4 + 4 + 4 + 4 + 4 + 4 + 4
+
+#include <iostream>
+#include <limits>
+
+using std::cout;
+using std::cin;
+using std::numeric_limits;
+using std::streamsize;
+
+int recursiveMultiplication(int, int);
+
+int main() {
+
+	cout << "=====================================\n";
+	cout << "20-05 RECURSIVE MULTIPLICATION DRIVER\n";
+	cout << "=====================================\n";
+	cout << "\n";
+
+	int value_1, value_2;
+
+	cout << "First number: ";
+	cin >> value_1;
+
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+	cout << "Second number: ";
+	cin >> value_2;
+
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+	cout << "\n";
+
+	cout << value_1 << " times " << value_2 << " via recursive multiplication = " << recursiveMultiplication(value_1, value_2) << ".\n";
+
+	cout << "\n";
+
+	cout << "Ending the program...\n";
+
+}
+
+int recursiveMultiplication(int x, int y) {
+
+	if (x == 0) {
+		return 0;
+	}
+	else {
+		return y + recursiveMultiplication(x - 1, y);
+	}
+
+
+}
